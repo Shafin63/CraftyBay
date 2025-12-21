202462a2b4d3a5fb132742517d4012b763aa6efe
Shafin <silentsk456@gmail.com>
2025-12-22 01:12:55 +0600

    add theme provider

---
+++ lib/app/providers/theme_provider.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier{
+  final String _themeKey = 'themeMode';
+
+  ThemeMode _currentThemeMode = ThemeMode.system;
+
+  ThemeMode get currentThemeMode => _currentThemeMode;
+
+  Future<void> loadInitialThemeMode() async {
+    ThemeMode themeMode = await _getThemeMode();
+    _currentThemeMode = themeMode;
+    notifyListeners();
+  }
+
+  void changeLocale(ThemeMode mode) {
+    if(currentThemeMode == mode) return;
+    _currentThemeMode = mode;
+    _saveThemeMode(mode.name);
+    notifyListeners();
+  }
+
+  Future<void> _saveThemeMode(String mode) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setString(_themeKey, mode);
+  }
+  Future<ThemeMode> _getThemeMode() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String savedThemeMode = sharedPreferences.getString(_themeKey) ?? '';
+    return getThemeModeFromString(savedThemeMode);
+  }
+}
+
+ThemeMode getThemeModeFromString(String value) {
+  switch(value) {
+    case 'light':
+      return .light;
+    case 'dark':
+      return .dark;
+    default:
+      return .system;
+  }
+}
+
+// extension ThemeExtension on ThemeMode {
+//   ThemeMode fromString(String value) {
+//     switch (value) {
+//       case 'light':
+//         return .light;
+//       case 'dark':
+//         return .dark;
+//       default:
+//         return .system;
+//     }
+//   }
+// }
